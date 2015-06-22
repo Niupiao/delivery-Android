d207d3ef2e47ab0804b8fe46e56b9a56479a6388
Ryan Kwon <rynkwn@gmail.com>
2015-06-22 12:07:59 -0400

    Add SlidingTab files

---
+++ app/src/main/java/com/niupiao/deliveryapp/MainTabActivity.java
@@ -0,0 +1,15 @@
+package com.niupiao.deliveryapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Inanity on 6/22/2015.
+ */
+public class MainTabActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        
+    }
+}
