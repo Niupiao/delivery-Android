da0bd4eeacfbb4b49fc3ff25d1c58b5a80f28d66
ohjoseph <josephoh.kr@gmail.com>
2015-06-25 15:14:22 -0400

    Messed up map

--- app/src/main/java/com/niupiao/deliveryapp/Map/MapFragment.java
+++ app/src/main/java/com/niupiao/deliveryapp/Map/MapFragment.java
@@ -1,22 +1,56 @@
 package com.niupiao.deliveryapp.Map;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.niupiao.deliveryapp.R;
 
 /**
  * Created by Inanity on 6/22/2015.
  */
-public class MapFragment extends android.support.v4.app.Fragment {
+public class MapFragment extends Fragment {
+
+    MapView mapView;
+    GoogleMap map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_map_tab, container, false);
 
+        mapView = (MapView) v.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        map = mapView.getMap();
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.setMyLocationEnabled(true);
+
         return v;
     }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 }
+
